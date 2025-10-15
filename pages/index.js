0984b1030440010d293223d39eb5ab0c7fc9ff19
Richard Lima Ribeiro <59461895+limarich@users.noreply.github.com>
2025-10-15 00:19:15 +0000

    add `prettier` and prettier scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,47 @@
 import { useEffect, useRef, useState } from "react";
-import styles from "./home.module.css"; 
+import styles from "./home.module.css";
 
 function Home() {
-       const videoRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(true);
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
 
-    const handleVideoClick = () => {
-        if (!videoRef.current) return;
-        videoRef.current.muted = false; 
+  const handleVideoClick = () => {
+    if (!videoRef.current) return;
+    videoRef.current.muted = false;
 
-        if (isPlaying) {
-            videoRef.current.pause();
-        } else {
-            videoRef.current.play();
-        }
-        setIsPlaying(!isPlaying);
-    };
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
 
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.muted = true; 
-            videoRef.current.play().catch((err) => {
-                console.warn("Autoplay bloqueado:", err);
-            });
-        }
-    }, []);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = true;
+      videoRef.current.play().catch((err) => {
+        console.warn("Autoplay bloqueado:", err);
+      });
+    }
+  }, []);
 
-    return (
-        <div className={styles["video-container"]}>
-            <video
-                ref={videoRef}
-                className={styles["background-video"]}
-                src="/boa-noite.mp4"
-                autoPlay
-                loop
-                playsInline
-                onClick={handleVideoClick}
-            />
-            <div className={styles["overlay-content"]}>
-                <h1>Bem-vindo à Home</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles["video-container"]}>
+      <video
+        ref={videoRef}
+        className={styles["background-video"]}
+        src="/boa-noite.mp4"
+        autoPlay
+        loop
+        playsInline
+        onClick={handleVideoClick}
+      />
+      <div className={styles["overlay-content"]}>
+        <h1>Bem-vindo à Home</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
