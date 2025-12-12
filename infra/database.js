aed02aaec40669854d71b97dc880d12dcfb87512
Richard Lima Ribeiro <59461895+limarich@users.noreply.github.com>
2025-12-12 11:57:30 +0000

    feat: configure database connection and update environment settings

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  client.end();
+  return result;
+}
+
+export default { query: query };
