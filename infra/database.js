23c340f591dc9c13dc402f008de018855adceb23
Richard Lima Ribeiro <59461895+limarich@users.noreply.github.com>
2025-12-19 21:41:43 +0000

    feat: update `.env` and connect the compose file to receive the envs

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_NAME,
   });
   await client.connect();
   const result = await client.query(queryObject);
